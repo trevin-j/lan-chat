09b87a779eec93edcc02b43fa742f7d76f3f900e
Trevin Jones <tr3vdev@gmail.com>
2023-07-17 16:09:49 -0600

    Docs and pylint update

--- client_ui.py
+++ client_ui.py
@@ -1,19 +1,26 @@
+"""
+Functions to display a clean client UI.
+"""
+
 import sys
 from os import get_terminal_size
+
 from lcsocket import LCSocket
+
 
 chat_history = ["\033[2K"]
 
-
 def print_chat():
+    """
+    Print out the messages in a nice manner.
+    """
     # Save cursor position
     print_nol("\033[s", flush=False)
 
-    for i in range(len(chat_history)):
+    for chat in chat_history:
         # Move cursor up one line and clear it
         print_nol("\033[F\033[2K\033[1G", flush=False)
-        print_nol(chat_history[i], flush=False)
-        # print_nol("\033[F") # <- get rid of \n from chat history
+        print_nol(chat, flush=False)
 
     print_nol((len(chat_history)) * "\033[E")
 
@@ -23,37 +30,40 @@ def print_chat():
 
 
 def add_chat(msg: str):
+    """
+    Add a chat to the chat history and display chats.
+    """
     for line in msg.split("\n"):
         chat_history.insert(0, line)
     print_chat()
 
 
 def print_nol(string: str, flush=True):
+    """
+    Print with no line.
+    """
     print(string, end="", flush=flush)
 
+
 def clear_current_line():
+    """
+    Clear the current line.
+    """
     sys.stdout.write("\033[2K\033[1G")
 
+
 def clear_last_line():
+    """
+    Clear the last line.
+    """
     sys.stdout.write("\033[F\033[2K\033[1G")
 
-def process_word_wrap(msg):
-    terminal_width = get_terminal_size()[0]
-    new_msgs = []
-    while True:
-        if len(msg) > terminal_width:
-            new_msgs.append(msg[:terminal_width]+"\n")
-            msg = msg[terminal_width:]
-        else:
-            new_msgs.append(msg)
-            break
-            
-    return new_msgs
 
-
-# Thread
 def msg_handler(client: LCSocket):
-    for i in range(get_terminal_size()[1]):
+    """
+    A thread function to receive messages and display them.
+    """
+    for _ in range(get_terminal_size()[1]):
         print()
 
     while True:
@@ -64,7 +74,7 @@ def msg_handler(client: LCSocket):
             client.close()
             add_chat("Host unexpectedly disconnected. Press enter to exit.")
             return
-        
+
         msg = data["message"]
         sender = data["source"]
 
